155f332f0cb36b36b14edfd0b02a0453d7232066
bhcao <c451735634@163.com>
2023-05-18 20:56:16 +0900

    init

---
+++ src/test/java/com/boheng/wiki/WikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boheng.wiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
