059f1f460d175ec903490d4ff098d0e8cea7ecfe
bhcao <c451735634@163.com>
2023-05-20 15:40:11 +0900

    add test.http

--- src/main/java/com/boheng/wiki/controller/TestController.java
+++ src/main/java/com/boheng/wiki/controller/TestController.java
@@ -1,7 +1,10 @@
 package com.boheng.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 public class TestController {
@@ -9,5 +12,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+
+    @PostMapping("/hello")
+    public String helloPost(String name) {
+        return "Hello World Post!" + name;
     }
 }
