f14161585c589c0672e70cfbf1dd97f847303806
bhcao <c451735634@163.com>
2023-05-20 15:11:18 +0900

    move application class to config package

--- src/main/java/com/boheng/wiki/config/WikiApplication.java
+++ src/main/java/com/boheng/wiki/config/WikiApplication.java
@@ -1,11 +1,13 @@
-package com.boheng.wiki;
+package com.boheng.wiki.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
 
+@ComponentScan("com.boheng")
 @SpringBootApplication
 public class WikiApplication {
 
