5d0bb09840aead8674f20c75709af70ee3cae30e
bhcao <c451735634@163.com>
2023-05-21 16:29:25 +0900

    ebook table + mybatis generator

---
+++ src/main/java/com/boheng/wiki/mapper/EbookMapper.java
@@ -0,0 +1,30 @@
+package com.boheng.wiki.mapper;
+
+import com.boheng.wiki.domain.Ebook;
+import com.boheng.wiki.domain.EbookExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface EbookMapper {
+    long countByExample(EbookExample example);
+
+    int deleteByExample(EbookExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Ebook record);
+
+    int insertSelective(Ebook record);
+
+    List<Ebook> selectByExample(EbookExample example);
+
+    Ebook selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByExample(@Param("record") Ebook record, @Param("example") EbookExample example);
+
+    int updateByPrimaryKeySelective(Ebook record);
+
+    int updateByPrimaryKey(Ebook record);
+}
