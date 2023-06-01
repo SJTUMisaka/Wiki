aa1a5ba2cd38956936cd64ea3c6df9dc9f9b40ef
bhcao <c451735634@163.com>
2023-06-01 21:12:28 +0900

    Add Filter to print time cost

---
+++ src/main/java/com/boheng/wiki/filter/LogFilter.java
@@ -0,0 +1,32 @@
+package com.boheng.wiki.filter;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class LogFilter implements Filter {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LogFilter.class);
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        LOG.info("------------- LogFilter Start -------------");
+        LOG.info("Request Addr: {} {}", request.getRequestURL().toString(), request.getMethod());
+        LOG.info("Remote Addr: {}", request.getRemoteAddr());
+
+        long startTime = System.currentTimeMillis();
+        filterChain.doFilter(servletRequest, servletResponse);
+        LOG.info("------------- LogFilter End Time：{} ms -------------", System.currentTimeMillis() - startTime);
+    }
+}
