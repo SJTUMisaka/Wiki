c3afec983ac9bb8cd59e2db47d7d812557b90534
bhcao <c451735634@163.com>
2023-06-09 22:54:58 +0900

    validation

--- src/main/java/com/boheng/wiki/req/PageReq.java
+++ src/main/java/com/boheng/wiki/req/PageReq.java
@@ -1,8 +1,14 @@
 package com.boheng.wiki.req;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+
 public class PageReq {
+    @NotNull(message = "Page should not be null")
     private int page;
 
+    @NotNull(message = "Size should not be null")
+    @Max(value = 1000, message = "Page size should be below 1000")
     private int size;
 
     public int getPage() {
--- web/src/views/admin/admin-ebook.vue
+++ web/src/views/admin/admin-ebook.vue
@@ -69,6 +69,7 @@
 <script lang="ts">
     import {defineComponent, onMounted, ref} from 'vue';
     import axios from 'axios';
+    import {message} from "ant-design-vue";
 
     export default defineComponent({
         name: 'AdminEbook',
@@ -76,7 +77,7 @@
             const ebooks = ref();
             const pagination = ref({
                 current: 1,
-                pageSize: 2,
+                pageSize: 2222,
                 total: 0
             });
             const loading = ref(false);
@@ -134,11 +135,14 @@
                 }).then((response) => {
                     loading.value = false;
                     const data = response.data;
-                    ebooks.value = data.content.list;
-
-                    // Reset Pagination
-                    pagination.value.current = params.page;
-                    pagination.value.total = data.content.total;
+                    if (data.success) {
+                        ebooks.value = data.content.list;
+                        // Reset Pagination
+                        pagination.value.current = params.page;
+                        pagination.value.total = data.content.total;
+                    } else {
+                        message.error(data.message);
+                    }
                 });
             };
 
